823ed76cd2a504ad6ddcff9d625f33abb82c88de
FranklinRiabani <fmercado@ucb.edu.bo>
2025-05-20 20:21:10 -0400

    conexion a base de datos  y .env

---
+++ src/Models/Clientes.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const clienteSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  apellido: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+});
+
+export const Cliente = mongoose.model('Cliente', clienteSchema);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Conexión a la base de datos MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Conectado a la base de datos MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error al conectar a la base de datos:', error);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hola Programacion Web I....');
